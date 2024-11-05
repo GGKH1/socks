7bfdb26386f9320031c0417ec69c8980ddfa1d12
GGKH1 <xijakadzegaga@gmail.com>
2024-11-05 04:03:16 +0400

    componentsPrompts

---
+++ components/ProductDisplay.js
@@ -0,0 +1,113 @@
+export default {
+  props: {
+    cart: {
+      type: Number,
+      required: true,
+    },
+    premium: {
+      // Ensure premium is defined as a prop
+      type: Boolean,
+      required: true,
+    },
+  },
+  template: `
+    <div class="product-display">
+      <div class="product-container">
+        <div class="product-image">
+          <img :class="{ 'out-of-stock-img': !inventory }" :src="image" />
+        </div>
+        <div class="product-info">
+          <h1>{{ title }}</h1>
+          <p>{{ stockStatus }}</p>
+          <p>Shipping: {{ shipping }}</p>
+          <ul>
+            <li v-for="detail in details" :key="detail">{{ detail }}</li>
+          </ul>
+          <div
+            v-for="(variant, index) in variants"
+            :key="variant.id"
+            @mouseover="updateVariant(index)"
+            class="color-circle"
+            :style="{ backgroundColor: variant.color }"
+          >
+            {{ variant.color }}
+          </div>
+          <button
+            class="button"
+            :class="{ disabledButton: !inventory }"
+            :disabled="!inventory"
+            @click="addToCart"
+          >
+            Add to Cart
+          </button>
+          <button
+            class="button"
+            @click="decrease"
+            :class="{ disabledButton: cart === 0 }"
+            :disabled="cart === 0"
+          >
+            Remove
+          </button>
+        </div>
+      </div>
+    </div>
+  `,
+  data() {
+    return {
+      product: "Socks",
+      brand: "G&G",
+      selectedVariant: 0,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        {
+          id: 2234,
+          color: "green",
+          image: "./assets/images/socks_green.jpg",
+          inventory: 10,
+        },
+        {
+          id: 2235,
+          color: "blue",
+          image: "./assets/images/socks_blue.jpg",
+          inventory: 0,
+        },
+      ],
+    };
+  },
+  computed: {
+    title() {
+      return `${this.brand} ${this.product}`;
+    },
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+    inventory() {
+      return this.variants[this.selectedVariant].inventory;
+    },
+    stockStatus() {
+      if (this.inventory === 0) {
+        return "Out of Stock";
+      } else if (this.inventory <= 10) {
+        return "Almost Sold Out";
+      } else {
+        return "In Stock";
+      }
+    },
+    shipping() {
+      return this.premium ? "Free" : "$2.99"; // Access premium here
+    },
+  },
+  methods: {
+    addToCart() {
+      if (this.inventory > 0) {
+        this.$emit("update-cart", 1);
+      }
+    },
+    decrease() {
+      this.$emit("update-cart", -1);
+    },
+    updateVariant(index) {
+      this.selectedVariant = index;
+    },
+  },
+};
--- main.js
+++ main.js
@@ -1,60 +1,18 @@
+import ProductDisplay from "./components/ProductDisplay.js";
+
 const app = Vue.createApp({
   data() {
     return {
       cart: 0,
-      product: "Socks",
-      brand: "G&G",
-      selectedVariant: 0,
-      details: ["50% cotton", "30% wool", "20% polyester"],
-      variants: [
-        {
-          id: 2234,
-          color: "green",
-          image: "../assets/images/socks_green.jpg",
-          inventory: 10,
-        },
-        {
-          id: 2235,
-          color: "blue",
-          image: "../assets/images/socks_blue.jpg",
-          inventory: 0,
-        },
-      ],
+      premium: true,
     };
   },
-  computed: {
-    title() {
-      return `${this.brand} ${this.product}`;
-    },
-    image() {
-      return this.variants[this.selectedVariant].image;
-    },
-    inventory() {
-      return this.variants[this.selectedVariant].inventory;
-    },
-    stockStatus() {
-      if (this.inventory === 0) {
-        return "Out of Stock";
-      } else if (this.inventory <= 10) {
-        return "Almost Sold Out";
-      } else {
-        return "In Stock";
-      }
-    },
+  components: {
+    ProductDisplay,
   },
   methods: {
-    addToCart() {
-      if (this.inventory > this.cart) {
-        this.cart += 1;
-      }
-    },
-    decrease() {
-      if (this.cart > 0) {
-        this.cart -= 1;
-      }
-    },
-    updateVariant(index) {
-      this.selectedVariant = index;
+    updateCart(amount) {
+      this.cart += amount;
     },
   },
 });
